71a22c2f6736885036e2d9ca729775eb3a489f69
Taiyob <oli1412001@gmail.com>
2023-08-19 17:53:11 +0600

    Problem File.......

---
+++ lib/clipR_Rect.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(SampleShortApp());
+}
+class SampleShortApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Text',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: ClipRRectWidget(),
+    );
+  }
+
+}
+
+class ClipRRectWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Clip R Rect'),
+      ),
+      body: Center(
+        child: ClipRect(
+            borderRadius: BorderRadius.circular(21),
+            child: Container(
+              color: Colors.lime,
+              height: 200,
+              width: 200,
+            ),
+        ),
+      ),
+    );
+  }
+
+}
