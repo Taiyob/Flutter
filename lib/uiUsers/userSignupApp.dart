43cca54c0c2b08d8541e383132a78c56a7827c0f
Taiyob <oli1412001@gmail.com>
2023-11-07 16:00:26 +0600

    registration ui

---
+++ lib/uiUsers/userSignupApp.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(UserSignUpForm());
+}
+
+class UserSignUpForm extends StatelessWidget {
+  const UserSignUpForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.white,
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Image(
+                          height: 50,
+                          width: 50,
+                          image: AssetImage('assets/images/logoLogin.png')),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Maintenance',
+                              style: TextStyle(
+                                  fontSize: 24.0,
+                                  fontWeight: FontWeight.w900,
+                                  color: Color(0xff203142))),
+                          Text('Box',
+                              style: TextStyle(
+                                  fontSize: 24.0,
+                                  fontWeight: FontWeight.w900,
+                                  color: Colors.orangeAccent)),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Column(
+                  children: [
+                    const Text('Sign Up',
+                        style: TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.black)),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                        'Interpreting oil analysis, \nreports is only meaningful.',
+                        style: TextStyle(
+                            fontSize: 12.0,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.grey)),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(
+                            Icons.account_circle,
+                            color: Color(0xff323f4b),
+                          ),
+                          hintText: 'Name',
+                          fillColor: Color(0xfff8f9f8),
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                const BorderSide(color: Color(0xffE4E7EB)),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                const BorderSide(color: Color(0xffE4E7EB)),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Contact',
+                          fillColor: const Color(0xfff8f9f8),
+                          filled: true,
+                          prefixIcon: const Icon(Icons.contact_phone,
+                              color: Color(0xff323f4b)),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide:
+                                const BorderSide(color: Color(0xffE4E7EB)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide:
+                                const BorderSide(color: Color(0xffE4E7EB)),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon:
+                              const Icon(Icons.email, color: Color(0xff323f4b)),
+                          fillColor: const Color(0xfff8f9f8),
+                          filled: true,
+                          hintText: 'Email',
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide:
+                                const BorderSide(color: Color(0xffE4E7EB)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide:
+                                const BorderSide(color: Color(0xffE4E7EB)),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          fillColor: const Color(0xfff8f9f8),
+                          filled: true,
+                          prefixIcon:
+                              const Icon(Icons.lock, color: Color(0xff323f4b)),
+                          suffixIcon: const Icon(Icons.visibility_off_outlined,
+                              color: Color(0xff323f4b)),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide:
+                                const BorderSide(color: Color(0xffE4E7EB)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide:
+                                const BorderSide(color: Color(0xffE4E7EB)),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 60,
+                    ),
+                    Container(
+                      height: 50,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        color: Colors.deepOrange,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: const Center(
+                        child: Text('SIGN UP',
+                            style: TextStyle(
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.w900,
+                                color: Colors.white)),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
